3813e23c272a599ffdb7167d1a1a7da3c17305f2
Delighted-Divide <flamingicebtw@gmail.com>
2023-10-03 15:33:23 +0530

    temp login added

---
+++ Phoenix/accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('<str:uname>/', views.log, name="Login")
+]
--- Phoenix/accounts/views.py
+++ Phoenix/accounts/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import login
+from django.shortcuts import redirect
 from django.shortcuts import render
+from .models import CustomUser
 
 # Create your views here.
+
+
+def log(request, uname):
+    user = CustomUser.objects.get(username=uname)
+    login(request, user)
+    return redirect('/')
