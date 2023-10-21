bcfc2cee88064247cce1bbe3f613622f9c17586a
Delighted-Divide <flamingicebtw@gmail.com>
2023-10-21 18:22:02 +0530

    doctor progress

--- Phoenix/static/doctor.js
+++ Phoenix/static/doctor.js
@@ -1,11 +1,13 @@
 function setProgress(percent) {
-  const circle = document.querySelector(".progress-ring__circle");
-  const circumference = 2 * Math.PI * circle.getAttribute("r");
-  const offset = circumference - (percent / 100) * circumference;
+  const circles = document.querySelectorAll(".progress-ring__circle");
+  circles.forEach((circle) => {
+    const circumference = 2 * Math.PI * circle.getAttribute("r");
+    const offset = circumference - (percent / 100) * circumference;
 
-  circle.style.strokeDasharray = `${circumference} ${circumference}`;
-  circle.style.strokeDashoffset = offset;
+    circle.style.strokeDasharray = `${circumference} ${circumference}`;
+    circle.style.strokeDashoffset = offset;
+  });
 }
 
 // Call this function with the desired percentage
-setProgress(50);
+setProgress(50); // for 50% progress
